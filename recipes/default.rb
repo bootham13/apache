042bc642d8c80d7b6b2ee9f4906c9bbd95a9f14e
Ubuntu <srikarpasham@gmail.com>
2020-03-31 19:32:03 +0000

    chef changes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,18 @@
-#
-# Cookbook:: apache
-# Recipe:: default
-#
-# Copyright:: 2020, The Authors, All Rights Reserved.
+apt_update
+
+package 'net-tools' do
+ action :install
+end
+
+package 'apache2' do
+ action :install
+end
+
+file '/var/www/html/index.html' do
+ action :create
+ content 'Hello pipeline World!'
+end
+
+service 'apache2' do
+ action [:enable, :start]
+end
