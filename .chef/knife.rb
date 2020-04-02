c84131ae2dfa15e692d1722b358a12cf0495179d
Ubuntu <srikarpasham@gmail.com>
2020-04-02 17:38:52 +0000

    commit files to azure

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -4,6 +4,6 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "srikarpasham"
-client_key               "#{current_dir}/srikarpasham.pem"
+client_key               "#{current_dir}/chefPem"
 chef_server_url          "https://api.chef.io/organizations/rsa_azure"
 cookbook_path            ["#{current_dir}/../cookbooks"]
